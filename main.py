62f8454262501b1ef7332e8f0a246ecadbd85b48
rtk0c <mail.tianyig@gmail.com>
2024-08-14 05:28:44 +0000

    Data storage, replying, add/subtract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sqlite3
 import discord
 
 # Let it throw error and crash if the env does not exist
@@ -17,19 +18,64 @@ async def on_ready():
 # Additionally allow dashes, because that's convenient
 VARNAME_REGEX = re.compile(r"[a-zA-Z0-9_-]+")
 
+db = sqlite3.connect('bot.db', autocommit=False)
+db_cur = db.cursor()
+
+db_cur.executescript(r"""
+CREATE TABLE IF NOT EXISTS variables(
+  guild_id INTEGER NOT NULL,
+  name,
+  value,
+  PRIMARY KEY (guild_id, name)
+);
+""")
+
+def var_get(guild_id: int, varname: str) -> int | None:
+  db_cur.execute(
+    r"SELECT value FROM variables WHERE guild_id = ? AND name = ?",
+    (guild_id, varname))
+  res = db_cur.fetchone()
+  if res:
+    (value,) = res
+    return value
+  else:
+    return None
+
+def var_set(guild_id: int, varname: str, value: int) -> None:
+  db_cur.execute(
+    r"INSERT OR REPLACE INTO variables(guild_id, name, value) VALUES (?, ?, ?)",
+    (guild_id, varname, value))
+  db.commit()
+
 @client.event
 async def on_message(message):
   if message.author == client.user or message.author.bot:
     return
 
-  c = message.content
-  if not c.startswith('++') and not c.startswith('--'):
+  if not message.guild:
     return
-  rest = c[2:]
-  if not VARNAME_REGEX.match(rest):
+  guild_id = message.guild.id
+
+  c = message.content
+  if c.startswith('++'):
+    delta = 1
+    varname = c[2:]
+  elif c.startswith('--'):
+    delta = -1
+    varname = c[2:]
+  else:
     return
 
-  print(f"recieved {rest}")
-  await message.reply(f"{rest} = 10")
+  if not VARNAME_REGEX.match(varname):
+    return
+
+  curr_value = var_get(guild_id, varname)
+  if curr_value:
+    new_value = curr_value + delta
+  else:
+    new_value = delta
+  var_set(guild_id, varname, new_value)
+
+  await message.reply(f"{varname} = {new_value}")
 
 client.run(client_token)
